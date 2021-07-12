21ad2fa3877562cceb8f215fda644401c8f27e95
Tristan Seifert <tristan@tseifert.me>
2021-07-11 20:55:26 -0500

    windowserver: Created RPC interface
    - Created the RPC interface applications will use to interact with the
      window server; currently, this just has some methods to submit events
      to the window server to be used by HID drivers.
    
    - Added a list of scancode identifiers for these events (they mirror the
      USB keyboard value types)

--- user/gui/windowserver/src/rpc/RpcServer.cpp
+++ user/gui/windowserver/src/rpc/RpcServer.cpp
@@ -4,16 +4,33 @@
 #include <rpc/rt/ServerPortRpcStream.h>
 
 /**
+ * Initializes the RPC server. A listening port will be opened and registered.
+ */
+RpcServer::RpcServer(const std::shared_ptr<Compositor> &comp) :
+    rpc::WindowServerServer(std::make_shared<rpc::rt::ServerPortRpcStream>(kPortName)) {
+    this->addCompositor(comp);
+}
+
+/**
  * Adds a compositor to the internal compositors list.
  */
 void RpcServer::addCompositor(const std::shared_ptr<Compositor> &comp) {
     REQUIRE(this->comps.empty(), "No support for multiple compositors yet");
 }
 
+
+
 /**
- * Message handling loop
+ * Handles a received key event.
  */
-int RpcServer::run() {
-    // TODO: implement
-    return -1;
+void RpcServer::implSubmitKeyEvent(uint32_t scancode, bool release) {
+    Trace("Key event: %5s %08x", release ? "break" : "make", scancode);
+}
+
+
+/**
+ * Handles a received mouse movement event.
+ */
+void RpcServer::implSubmitMouseEvent(uint32_t buttons, int32_t dX, int32_t dY, int32_t dZ) {
+    Trace("Mouse event: buttons %08x offsets %4d %4d %4d", buttons, dX, dY, dZ);
 }
--- user/gui/windowserver/src/rpc/RpcServer.h
+++ user/gui/windowserver/src/rpc/RpcServer.h
@@ -4,26 +4,30 @@
 #include <string_view>
 #include <vector>
 
+#include "Server_WindowServer.hpp"
+
 class Compositor;
 
 /**
  * Provides the window server's RPC interface, which applications use to create windows on screen.
  */
-class RpcServer {
+class RpcServer: public rpc::WindowServerServer {
     public:
         /// Name under which the RPC port is registered
         constexpr static const std::string_view kPortName{"me.blraaz.rpc.windowserver"};
 
     public:
         /// Initializes the RPC server with the given compositor.
-        RpcServer(const std::shared_ptr<Compositor> &comp) {
-            this->addCompositor(comp);
-        }
+        RpcServer(const std::shared_ptr<Compositor> &comp);
+        /// Cleans up the RPC server.
+        virtual ~RpcServer() = default;
 
         /// Registers a compositor, which is responsible for a particular display.
         void addCompositor(const std::shared_ptr<Compositor> &comp);
-        /// Executes the message processing loop.
-        int run();
+
+    protected:
+        void implSubmitKeyEvent(uint32_t scancode, bool release) override;
+        void implSubmitMouseEvent(uint32_t buttons, int32_t dX, int32_t dY, int32_t dZ) override;
 
     private:
         /// All active compositors
